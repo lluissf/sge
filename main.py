4634aa54ffe6fc4c73830c9f70e48051b0d10471
Luís Felipe <luis.felipe.ferreira.da.silva.if@gmail.com>
2026-04-16 15:29:56 -0300

    feat: :sparkles: Adicionando os Produtos

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
     Sistema de Gestão de Estoque.
     Desenvolvido por: @lluissf
 """
+# Criando os produtos
+produtos = [
+    {"id": 1, "nome": "Teclado Gamer", "preco": 100.29, "quantidade": 1},
+    {"id": 2, "nome": "Mouse Gamer", "preco": 76, "quantidade": 2},
+    {"id": 3, "nome": "Headset Gamer", "preco": 70.3, "quantidade": 10}
+]
