8a767eee2882132d58694e243a62c78a606304d0
Luís Felipe <luis.felipe.ferreira.da.silva.if@gmail.com>
2026-04-16 15:34:31 -0300

    feat: :sparkles: Adicionando funcionalidade de menu para listar produtos

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ produtos = [
     {"id": 2, "nome": "Mouse Gamer", "preco": 76, "quantidade": 2},
     {"id": 3, "nome": "Headset Gamer", "preco": 70.3, "quantidade": 10}
 ]
+
+# Criando o Menu.
+while True:
+    print(f"===================\n")
+    print(f"1. Listar Produtos.\n")
+    print(f"0. Sair do Programa.\n")
+    print(f"===================\n")
+
+    opcao = int(input("Insira o número: "))
+
+    match opcao:
+        case 0:
+            break
+        case 1:
+            print(f"Listar Produtos")
+
+print(f"Programa Encerrado.")
